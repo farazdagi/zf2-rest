4956ee4f5ece2228b217ab03881e999a3f4b03ac
Victor Farazdagi <simple.square@gmail.com>
2011-11-09 11:42:56 +0100

    Update auto loader

--- modules/Gists/autoload_classmap.php
+++ modules/Gists/autoload_classmap.php
@@ -1,3 +1,6 @@
 <?php
 return array(
+  'Gists\\Controller\\IndexController' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Gists' . DIRECTORY_SEPARATOR . 'Controller' . DIRECTORY_SEPARATOR . 'IndexController.php',
+  'Gists\\Module' => __DIR__ . DIRECTORY_SEPARATOR . 'Module.php',
+  'Gists\\Mvc\\Controller\\RestfulController' =>  __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Gists' . DIRECTORY_SEPARATOR . 'Mvc' . DIRECTORY_SEPARATOR . 'Controller' . DIRECTORY_SEPARATOR . 'RestfulController.php',
 );
